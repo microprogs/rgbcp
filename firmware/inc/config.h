ad94fa82fbf8ad323ad01c183b607dca1b3de0df
microprogs <microprogs@gmail.com>
2014-02-16 01:27:21 +0400

    config

---
+++ firmware/inc/config.h
@@ -0,0 +1,24 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define EXT_CRYSTAL_CLOCK	12000000
+#define CPU_CORE_CLOCK		48000000
+
+//#define LED_RED_PORT		3
+//#define LED_RED_BIT			0
+
+//#define LED_GREEN_PORT		3
+//#define LED_GREEN_BIT		1
+
+//#define SWITCH_DBG_PORT		3
+//#define SWITCH_DBG_BIT		2
+
+
+#define SystemCoreClock CPU_CORE_CLOCK
+
+
+
+
+
+#endif
+

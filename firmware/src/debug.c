acda98aa9a94eaae7ed2e1999a9542956d2cfd69
microprogs <microprogs@gmail.com>
2014-02-16 01:19:50 +0400

    debug

---
+++ firmware/src/debug.c
@@ -0,0 +1,23 @@
+#include <debug.h>
+#include <leds.h>
+#include <delay.h>
+
+uint8_t debug_buf0[DEBUG_BUF_SIZE];
+uint8_t debug_buf1[DEBUG_BUF_SIZE];
+uint8_t debug_buf2[DEBUG_BUF_SIZE];
+uint8_t debug_buf3[DEBUG_BUF_SIZE];
+
+void fatal_error()
+{
+	while (1)
+	{
+//		led_red_power_on();
+//		led_green_power_off();
+//		delay_ticks(2000000);
+
+//		led_red_power_off();
+//		led_green_power_on();
+//		delay_ticks(2000000);
+	}
+}
+
